08403e2a2f4b00a0f10eee13b807adf236580ff2
ashwinii-gv <ashwinigv237@gmail.com>
2024-09-20 23:27:51 +0530

    First commit

---
+++ OnlineShopping/src/com/tns/onlineshopping/entities/Admin.java
@@ -0,0 +1,8 @@
+package com.tns.onlineshopping.entities;
+
+public class Admin extends User{
+	public Admin(int userId, String username, String email, String address) {
+		super(userId, username,email,address);
+	}
+
+}
